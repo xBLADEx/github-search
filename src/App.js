9e19cd8c2240ee8873f65582134dc00b045246ca
Rich Edmunds <rich@richedmunds.com>
2022-03-21 17:10:20 -0600

    feat: Add working search and display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import Fetch from './utilities/fetch-wrapper';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState('');
+  const [repos, setRepos] = useState([]);
+  const API = new Fetch('https://api.github.com/');
+
+  async function handleFormSubmit(event) {
+    event.preventDefault();
+
+    // Bail early, extra safety.
+    if (!user.length > 0) {
+      return;
+    }
+
+    const result = await API.get(`users/${user}/repos`).then((data) => data);
+    setRepos(result);
+    setUser('');
+  }
+
+  function handleUserChange(event) {
+    setUser(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="row">
+        <header className="header">
+          <h1 className="heading">Search Repositories</h1>
+        </header>
+
+        <form onSubmit={handleFormSubmit} className="form">
+          <label htmlFor="user" className="label">
+            Enter GitHub User
+          </label>
+          <input type="text" id="user" name="user" value={user} onChange={handleUserChange} className="input" />
+
+          <input type="submit" value="Search" className="button" disabled={user.length === 0} />
+        </form>
+
+        {repos.length > 0 && (
+          <ul className="repos">
+            {repos.map(({ html_url, name, id }) => (
+              <li key={id}>
+                <a href={html_url} target="_blank" rel="noopener noreferrer" className="repo">
+                  <h2 className="repo-heading">{name.replaceAll('-', ' ')}</h2>
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
